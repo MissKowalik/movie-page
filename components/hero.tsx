95ae1a53cc61a8c0a67747735bb0ee1766e5b9a7
Linda <lindakowalik@gmail.com>
2025-10-09 16:53:51 +0200

    render genres and release year in hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,21 @@
-import { getPopularMovies } from "@/lib/data/tmdb"
+import { getGenres, getPopularMovies } from "@/lib/data/tmdb"
 import Image from "next/image";
+
+type Genre = {
+    name: string,
+    id: number,
+}
 
 export default async function Hero() {
     const movies = await getPopularMovies();
-    const heroMovie = await movies[4];
+    const heroMovie = await movies[2];
+    const genres = await getGenres();
+    
+    // map genre ids to names
+    const genreNames = heroMovie.genre_ids.map((id: number) => genres.find((g: Genre) => g.id === id)?.name)
+
+    // Extract release year from release_date string
+    const releaseYear = heroMovie.release_date.split("-")[0];
 
     return (
     <header className="relative w-full h-[calc(100vh-80px)]">
@@ -15,9 +27,25 @@ export default async function Hero() {
             sizes="100vw"
             priority
         />
+
         <div className="absolute bottom-0 left-0 w-full h-200 bg-gradient-to-t to-transparent from-black" />
         <div className="absolute bottom-45 left-1/10 max-w-[40%] p-4 rounded-md text-white">
-            <h1 className="text-6xl">{heroMovie.title}</h1>
+            <h1 className="text-6xl">
+                {heroMovie.title}
+                <span className="ml-6 text-2xl font-light align-baseline">{releaseYear}</span>
+            </h1>
+
+            {/* genres */}
+            <div className="flex gap-4 flex-wrap mt-2">
+                {genreNames.map((genre: string) => (
+                    <span
+                        key={genre}
+                        className="px-2 py-1 bg-gray-600/60 rounded text-xl"
+                    >
+                        {genre}
+                    </span>
+                ))}
+            </div>
         </div>
     </header>
   );
