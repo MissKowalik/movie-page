806b7a577698c1ed4514b64050b019e0acbe7156
Linda <lindakowalik@gmail.com>
2025-10-09 13:24:05 +0200

    added functions to fetch popular and upcoming movies from TMDB

---
+++ lib/data/tmdb.ts
@@ -0,0 +1,36 @@
+export async function getPopularMovies() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?&page=1&api_key=${process.env.API_KEY}`,
+    {
+      headers: {
+        accept: "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`API responded with status ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.results;
+}
+
+
+export async function getUpcomingMovies() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/upcoming?&page=1&api_key=${process.env.API_KEY}`,
+    {
+      headers: {
+        accept: "application/json",
+      },
+    }
+  );
+
+  if (!response.ok) {
+    throw new Error(`API responded with status ${response.status}`);
+  }
+
+  const data = await response.json();
+  return data.results;
+}
