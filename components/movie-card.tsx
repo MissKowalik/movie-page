dbc9f0d9fca8d60dc6a893794fe4f49051c0b48b
Linda <lindakowalik@gmail.com>
2025-10-13 16:22:20 +0200

    adjusted component width and padding classes for better responsiveness

--- components/movie-card.tsx
+++ components/movie-card.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 
 export default async function MovieCard() {
     const popularMovies = await getPopularMovies();
-    const movie = await popularMovies[1];
+    const movie = await popularMovies[19];
     const release_year = await movie.release_date.split("-")[0];
 
     return (
-        <article className="min-w-4xs max-w-xs">
+        <article className="min-w-[150px] max-w-[250px]">
             <Image
                 src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
                 alt={`poster of ${movie.title}`}
@@ -15,7 +15,7 @@ export default async function MovieCard() {
                 height={750}
                 className="rounded"
             />
-            <div className="py-2 text-xl">
+            <div className="p-2 text-xl">
                 <div className="flex justify-between gap-4 text-xs lg:text-lg">
                     <span>{movie.vote_average.toFixed(1)}</span>
                     <span>{release_year}</span>
