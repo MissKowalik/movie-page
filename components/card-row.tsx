aa1fb36e963d6a44f151a3951e79c542f9308132
Linda <lindakowalik@gmail.com>
2025-10-17 10:33:24 +0200

    card row: improved layout with grid and semantic HTML

--- components/card-row.tsx
+++ components/card-row.tsx
@@ -2,19 +2,21 @@ import { Movie } from "@/lib/interfaces/movie";
 import MovieCard from "./movie-card";
 
 type CardRowProps = {
-    heading: string;
-    movies: Movie[];
+  heading: string;
+  movies: Movie[];
 };
 
 export default function CardRow({ heading, movies }: CardRowProps) {
-    return (
-        <section className="px-4 md:px-8 py-8 overflow-hidden">
-            <h2 className="text-xl lg:text-3xl pb-8">{heading}</h2>
-            <div className="flex gap-4 overflow-x-auto scrollbar-hide">
-                {movies.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-            </div>
-        </section>
-    );
+  return (
+    <section className="px-4 md:px-8 py-8 overflow-hidden">
+      <h2 className="text-xl lg:text-3xl pb-8">{heading}</h2>
+      <ul className="grid grid-flow-col auto-cols-[minmax(150px,1fr)] gap-4 overflow-x-auto scrollbar-hide">
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <MovieCard movie={movie} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
