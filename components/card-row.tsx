f9a47b4b0514fc2b39474b9df23d9b2975231f4f
Linda <lindakowalik@gmail.com>
2025-10-17 13:57:47 +0200

    cardRow: created buttons for side scroll visually

--- components/card-row.tsx
+++ components/card-row.tsx
@@ -1,5 +1,6 @@
 import { Movie } from "@/lib/interfaces/movie";
 import MovieCard from "./movie-card";
+import Image from "next/image";
 
 type CardRowProps = {
   heading: string;
@@ -8,15 +9,39 @@ type CardRowProps = {
 
 export default function CardRow({ heading, movies }: CardRowProps) {
   return (
-    <section className="px-4 md:px-8 py-8 overflow-hidden">
+    <section className="relative px-4 md:px-8 py-8 overflow-hidden">
       <h2 className="text-xl lg:text-3xl pb-8">{heading}</h2>
-      <ul className="grid grid-flow-col auto-cols-[minmax(150px,1fr)] gap-4 overflow-x-auto scrollbar-hide">
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <MovieCard movie={movie} />
-          </li>
-        ))}
-      </ul>
+
+        {/* left scroll button */}
+        <button className="absolute left-[3%] top-1/2 -translate-y-1/2 z-10 p-2 rounded-full bg-black/70 hover:bg-black/90 transition hover:cursor-pointer">
+            <Image
+                src="/arrow-backward.svg"
+                alt="Scroll left"
+                width={20}
+                height={20}
+                className="invert"
+            />
+        </button>
+
+        {/* card list */}
+        <ul className="grid grid-flow-col auto-cols-[minmax(150px,1fr)] gap-4 overflow-x-auto scrollbar-hide">
+            {movies.map((movie) => (
+            <li key={movie.id}>
+                <MovieCard movie={movie} />
+            </li>
+            ))}
+        </ul>
+
+        {/* right scroll button */}
+        <button className="absolute right-[3%] top-1/2 -translate-y-1/2 z-10 p-2 rounded-full bg-black/70 hover:bg-black/90 transition hover:cursor-pointer">
+            <Image
+                src="/arrow-forward.svg"
+                alt="Scroll right"
+                width={20}
+                height={20}
+                className="invert"
+            />
+        </button>
     </section>
   );
 }
